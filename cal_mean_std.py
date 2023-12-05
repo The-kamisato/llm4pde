4616a03eb4a99ff899477e9c9d115b1fca43d6b4
Bokai Lin <139728563+zhuanrangqun@users.noreply.github.com>
2023-12-05 22:45:42 +0800

    Update cal_mean_std.py

--- cal_mean_std.py
+++ cal_mean_std.py
@@ -17,22 +17,24 @@ def cal_mean_std(surface_folder_path = surface_folder_path, upper_folder_path = 
     surface_mean_list = torch.zeros(len(file_list), 4)
     # 逐个读取文件并将数据存入列表
     for i, file in enumerate(file_list):
+        print("surface_mean", i)
         file_path = os.path.join(surface_folder_path, file)
-        data_tensor = nc_surface_loader(file_path)      #  (4, day, 1440, 721)      day 是一个月的天数
+        data_tensor = nc_surface_loader(file_path)      #  (day, 4, 1440, 721)      day 是一个月的天数
 
         surface_mean_list[i] = torch.mean(data_tensor, dim = (1, 2, 3))
         
     all_surface_mean = torch.mean(surface_mean_list, dim = 0)                   # (4, )的张量
 
     # 标准差的计算
-    surface_std_list = torch.zeros(len(file_list), 4)
+    surface_var_list = torch.zeros(len(file_list), 4)
     for i, file in enumerate(file_list):
+        print("surface_std", i)
         file_path = os.path.join(surface_folder_path, file)
         data_tensor = nc_surface_loader(file_path)      #  (4, day, 1440, 721)
         
-        surface_std_list[i] = torch.sqrt(torch.mean(((data_tensor.permute(1, 2, 3, 0) - all_surface_mean) ** 2), dim = (0, 1, 2)))
+        surface_var_list[i] = torch.mean(((data_tensor.permute(1, 2, 3, 0) - all_surface_mean) ** 2), dim = (0, 1, 2))
 
-    all_surface_std = torch.mean(surface_std_list, dim = 0)  
+    all_surface_std = torch.sqrt(torch.mean(surface_var_list, dim = 0))  
 
 
     # 计算upper数据的所有均值和方差
@@ -43,6 +45,7 @@ def cal_mean_std(surface_folder_path = surface_folder_path, upper_folder_path = 
     upper_mean_list = torch.zeros(len(file_list), 5, 13)            # 注意到它5个变量随着高度变化挺大，因此分别求出相应的均值和标准差
 
     for i, file in enumerate(file_list):
+        print("upper_mean", i)
         file_path = os.path.join(upper_folder_path, file)
         data_tensor = nc_upper_loader(file_path)                    # (5, 13, 1440, 721)
         
@@ -53,6 +56,7 @@ def cal_mean_std(surface_folder_path = surface_folder_path, upper_folder_path = 
     # 标准差的计算
     upper_std_list = torch.zeros(len(file_list), 5, 13)            # 注意到它5个变量随着高度变化挺大，因此分别求出相应的均值和标准差
     for i, file in enumerate(file_list):
+        print("upper_std", i)
         file_path = os.path.join(upper_folder_path, file)
         data_tensor = nc_upper_loader(file_path)                    # (5, 13, 1440, 721)
         
@@ -66,4 +70,9 @@ if __name__ == "__main__":
     all_surface_mean, all_surface_std, all_upper_mean, all_upper_std = cal_mean_std()
     print(all_surface_mean)
     print(all_upper_mean)
+    
+    torch.save(all_surface_mean, '/home/lbk/llm/llama4pde/data/surface_mean.pt')
+    torch.save(all_surface_std, '/home/lbk/llm/llama4pde/data/surface_std.pt')
+    torch.save(all_upper_mean, '/home/lbk/llm/llama4pde/data/upper_mean.pt')
+    torch.save(all_upper_std, '/home/lbk/llm/llama4pde/data/upper_std.pt')
     
